c66d0b191c78237b9f70c528a76a865e8cdbb1cb
joseph ikinda <josephikinda@gmail.com>
2025-07-21 00:14:29 +0200

    006 Implement

--- app/routes.ts
+++ app/routes.ts
@@ -4,5 +4,6 @@ export default [
     index("routes/home.tsx"),
     route("/auth", "routes/auth.tsx"),
     route("/upload", "routes/upload.tsx"),
+    route("/wipe", "routes/wipe.tsx"),
     route("/resume/:id", "routes/resume.tsx"),
 ] satisfies RouteConfig;
